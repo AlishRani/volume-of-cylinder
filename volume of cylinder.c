31756c85ece42ccb9345cc4001add23f846679c5
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-11-28 18:51:38 +0530

    Add files via upload

---
+++ volume of cylinder.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   float pi=3.14;
+   float r;
+   float h;
+   printf("enter the value of r\n);
+   scanf("%f",&r);
+   printf("enter the value of h\n);
+   scanf("%f",&h);
+   float a=pi*r*r*h;
+   printf("volume of cylinder%f\n",a);
+   return 0;
+}
+   
